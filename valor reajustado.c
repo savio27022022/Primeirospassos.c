6d7e8aa2e077b5b21ad8acbf3271011c618c3b05
Sávio <115809910+savio27022022@users.noreply.github.com>
2024-02-28 14:30:59 -0300

    Update valor reajustado.c

--- valor reajustado.c
+++ valor reajustado.c
@@ -1,42 +1,42 @@
-include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
 
+void cabecalho(){
+printf("\n===SENAI====\n");
+system("cls||clear");
+fflush(stdin);
+}
 
 
 int main() {
-
+setlocale(LC_ALL,"portuguese");
+ //Declarando as Variáveis.
  float valor, reajuste, novoValor;
 
-
-
  // Leitura do valor original
-
+cabecalho();
  printf("Digite um valor: ");
-
  scanf("%f", &valor);
 
 
 
- // Cálculo do reajuste (10%)
-
+ // Cálculo do reajuste (10%).
+ cabecalho();
  reajuste = valor * 0.1;
-
-
-
+ 
  // Cálculo do novo valor
-
+ cabecalho();
  novoValor = valor + reajuste;
 
 
-
  // Exibição do valor original, reajuste e novo valor
-
+cabecalho();
  printf("Valor original: R$ %.2f\n", valor);
-
  printf("Reajuste: R$ %.2f (10%%)\n", reajuste);
-
+ 
+cabecalho();
  printf("Novo valor: R$ %.2f\n", novoValor);
-
-
 
  return 0;
 
