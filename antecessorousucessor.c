7272a009aa7136d7e1bbcf41411903d570cca23f
Sávio <115809910+savio27022022@users.noreply.github.com>
2024-02-28 13:31:52 -0300

    Update antecessorousucessor.c

--- antecessorousucessor.c
+++ antecessorousucessor.c
@@ -1,17 +1,30 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+void cabecalho(){
+printf("\n=== SENAI ===\n);
+system("cls||clear");
+fflush(stdin);
+}
+
 
 int main() {
-  int numero;
+setlocale(LC_ALL,"portuguese");
+int numero;
 
-  // Leitura do número inteiro
+  // Leitura do número inteiro.
+  cabecalho();
   printf("Digite um número inteiro: ");
   scanf("%d", &numero);
 
-  // Cálculo do antecessor e sucessor
+  // Cálculo do antecessor e sucessor.
+  cabecalho();
   int antecessor = numero - 1;
   int sucessor = numero + 1;
 
-  // Exibição do antecessor e sucessor
+  // Exibição do antecessor e sucessor.
+  cabecalho();
   printf("Antecessor: %d\n", antecessor);
   printf("Sucessor: %d\n", sucessor);
 
